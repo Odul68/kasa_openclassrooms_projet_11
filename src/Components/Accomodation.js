8c5cf9a4c9d108c785cc578fda6b66742b8b6f26
ludovic <ludovic.deckert@gmail.com>
2022-08-16 17:51:26 +0800

    last change

--- src/Components/Accomodation.js
+++ src/Components/Accomodation.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-export default function Accomodation(accomodation) {
+export default function Accomodation({ one }) {
   const [data, setData] = useState([]);
 
   const getData = () => {
